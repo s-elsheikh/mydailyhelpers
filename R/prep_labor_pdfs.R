d7dc6b2d9a03713d07fde6ab51d55f5190cdcccd
samer <samer.elsheikh@uniklinik-freiburg.de>
2024-07-25 10:36:34 +0200

    sometimes two columns same timstamp: included auftragsnummer, then removed. Defining margins is still problematic, using date x-coord as some columns are very wide

--- R/prep_labor_pdfs.R
+++ R/prep_labor_pdfs.R
@@ -8,7 +8,7 @@
 #' @param stop_term stringr::regex() expression to stop at if encountered
 #' @param verbose prints piz and page number of the dicument being processed to console
 #'
-#' @return a named list (after labor parameters) containing date_time and value
+#' @return a tibble containing date_time and value
 #' @export
 #'
 #' @examples
@@ -100,6 +100,10 @@ prep_labor_pdf_pages <- function(pdf_df,
    lab_table <- relevant_prepped_page %>%
         # group right left entries according to positions of
         # date format strings from relevant_coords
+        # sometimes is the auftragsnummer longer than the date
+        # and projects to the left, so i have to decrease at least 4
+        # from x coords of the date to get the correct Auftragsnummer
+        # for the date. Either decrease by 6 or coallece in post proc
         dplyr::mutate(
             grp = cut(x, c(unique(relevant_coords$x), Inf), right = FALSE)
         )  %>%
@@ -119,17 +123,21 @@ prep_labor_pdf_pages <- function(pdf_df,
        # add standard naming for next step
        `names<-`(letters[1:dim(.)[2]])
 
-
+   # browser()
    # take colnames from upper rows
    names_of_col <- lab_table %>%
        dplyr::filter(
            stringr::str_detect(a, "^Auftrag") |
-               b == "bereich"
-           )%>%
+               b == "bereich" |
+               # some pats have 2 columns with the
+               # same time stamp, si i included
+               # auftragsnummer
+               dplyr::lag(a) == "Auftrags-Nr."
+       ) %>%
        # map along cols, to remove nas
        # and collapse then clean
        purrr::map(~ stats::na.omit(.x) %>%
-               stringr::str_c(collapse = " ")
+                      stringr::str_c(collapse = "_")
        ) %>%
        unlist() %>%
        stringr::str_replace("-", "_") %>%
@@ -141,7 +149,9 @@ prep_labor_pdf_pages <- function(pdf_df,
     pivoted_tidy_lab_tib <- lab_table %>%
         # filter for rows, not taken for naming
         dplyr::filter(!(stringr::str_detect(a, "^Auftrag") |
-                b == "bereich" )
+                b == "bereich"  |
+                    dplyr::lag(a) == "Auftrags-Nr."
+                )
 
         ) %>%
         `names<-`(names_of_col)   %>%
@@ -149,108 +159,27 @@ prep_labor_pdf_pages <- function(pdf_df,
         tidyr::pivot_longer(!c(1:2),
                      names_to = "date_time") %>%
         dplyr::mutate(
+            date_time = stringr::str_remove(date_time, "_\\d+$"),
             date_time = lubridate::dmy_hm(date_time)
         )  %>%
-        dplyr::filter(value != "empty")
+        dplyr::filter(value != "empty") %>%
+        dplyr::filter(stringr::str_detect(auftrag, wanted_labs))
 
-    #
-    # prepped_page_with_dummy <- relevant_prepped_page %>% # added dummy entry, not to lose dates
-    #     dplyr::bind_rows(
-    #         tibble::tibble_row(x = relevant_coords$x[1] +1 ,
-    #                    y = relevant_coords$y[2],
-    #                    text =  "datum")
-    #     )
-    #
-    # #browser()
-    # split_page <- prepped_page_with_dummy %>%
-    #     dplyr::mutate(
-    #         grp = cut(x, c(unique(relevant_coords$x), Inf), right = FALSE)
-    #     ) %>%
-    #     dplyr::filter(
-    #         !is.na(grp) &
-    #             y > relevant_coords$y[1]
-    #     ) %>%
-    #     split(.$grp)
-    #
-    # # browser()
-    # to_reduce_list <-  split_page %>%
-    #     purrr::map( function(df) {
-    #
-    #         out <- df %>%
-    #             dplyr::arrange(y, x) %>%
-    #             dplyr::group_by(y) %>%
-    #             tidyr::pivot_wider(names_from = x, values_from = text, names_sort = TRUE) %>%
-    #             tidyr::unite(
-    #                 auftrag, where(is.character), sep = " ") %>%
-    #             dplyr::mutate(
-    #                 auftrag = stringr::str_remove_all(auftrag, "NA") %>% stringr::str_squish()) %>%
-    #             dplyr::select(-grp)
-    #         return(out)
-    #     }
-    #     )
-    #
-    # out <- to_reduce_list %>%
-    #     purrr::reduce(~ dplyr::left_join(.x, .y, by= "y")) %>%
-    #     purrr::when (dim(.)[2]> 2 ~ tidyr::unite(., wert, 2:3),
-    #           dim(.)[2]< 3 ~ dplyr::rename(., wert = auftrag) ) %>%
-    #     dplyr::filter(stringr::str_detect(wert, wanted_labs)|
-    #                stringr::str_detect(wert, "datum_bereich|Auftrags-Nr"))
-    # #browser()
-    # if(dim(out)[2] >= 3) {
-    #
-    #     new_names <- out %>% dplyr::ungroup() %>%
-    #         dplyr::slice_head(n = 2) %>%
-    #         dplyr::mutate(
-    #             dplyr::across(tidyselect::everything(), ~ stringr::str_c(.x, dplyr::lead(.x), sep = "_"))
-    #         ) %>% tidyr::unite(new_names, 3:tidyselect::last_col(), sep = "###") %>%
-    #         .[1,3] %>%
-    #         dplyr::pull() %>%
-    #         stringr::str_split(., "###") %>%
-    #         unlist()
-    #
-    #     # check for duplicate colnames
-    #     while(sum(duplicated(new_names)) != 0) {
-    #
-    #         new_number <- stringr::str_sub(new_names, -1, -1) %>% as.integer()
-    #
-    #
-    #         new_number[duplicated(new_names)] <- new_number[duplicated(new_names)] + 1
-    #
-    #         new_names <- stringr::str_replace(new_names, "\\d$", as.character(new_number))
-    #
-    #     }
-    #
-    #
-    #
-    #     new_names <- c("y", "wert", new_names)
-    #
-    #     names(out) <- new_names
-    #     out <- out[c(-1, -2),-1]
-    #
-    # }
-    #
-    # sum_col_names <-
-    #     names(out) %>%
-    #     stringr::str_detect("(\\d{2}\\.){2}\\d{2}_\\d{2}:\\d\\d") %>%
-    #     sum()
-    #
-    # if(sum_col_names == 0| is.na(sum_col_names)) return()
-    #
-    # #browser()
-    # out <- out %>%
-    #     tidyr::pivot_longer(2:tidyselect::last_col(), names_to = "date_time", values_to = "value") %>%
-    #     dplyr::distinct() %>%
-    #     dplyr::mutate(
-    #         dplyr::across(where(is.character), dplyr::na_if, "empty"),
-    #         date_time = lubridate::dmy_hm(date_time)
-    #     ) %>%
-    #     tidyr::drop_na() %>%
-    #     split(., .$wert) %>%
-    #     purrr::map(dplyr::select, - wert)
 
+    # tidy output
     out <- pivoted_tidy_lab_tib %>%
-        group_by(auftrag) %>%
-        group_split()
+        mutate(
+            num_value = str_extract(value, "[0-9,]*") %>%
+                str_replace(",", ".") %>%
+                as.numeric(),
+            opinion = case_when(
+                str_detect(value, "-") ~ "lo",
+                str_detect(value, "\\+") ~ "hi",
+                TRUE ~ "normal"
+            )
+        )
+    # there are inconsistencies in Referenzbereich
+
     return(out)
 
 }
